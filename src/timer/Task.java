944b9006943daaae18bae7ad032a66b563d5426c
Matt Bailey <baimy017@mymail.unisa.edu.au>
2020-09-22 13:31:31 +0930

    Added Duration and Schedule class
    Added private variables and getters/setters to Task class.

--- src/timer/Task.java
+++ src/timer/Task.java
@@ -2,4 +2,22 @@ package timer;
 
 public class Task {
 
+	// Instance Variables
+	private Duration duration;
+	private String name;
+	
+	
+	// Getters and Setters
+	public Duration getDuration() {
+		return duration;
+	}
+	public void setDuration(Duration duration) {
+		this.duration = duration;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
