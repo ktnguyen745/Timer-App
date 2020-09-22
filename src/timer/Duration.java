ecd2ae4d80323a9dfa81bbc414e465f4299c7d30
nguky053 <kristynguyen745@gmail.com>
2020-09-22 14:11:19 +0930

    Create a 20 second countdown timer

--- src/timer/Duration.java
+++ src/timer/Duration.java
@@ -1,5 +1,27 @@
 package timer;
 
-public class Duration {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class Duration {
+	static int interval = 20;
+	static Timer timer = new Timer();
+    private int delay = 1000;
+    private int period = 1000;
+
+	Duration(){
+		timer.scheduleAtFixedRate(new TimerTask() {
+
+	        public void run() {
+	            System.out.println(setInterval());
+
+	        }
+	    }, delay, period);	
+	}
+
+	private static final int setInterval() {
+	    if (interval == 1)
+	        timer.cancel();
+	    return --interval;
+	}
 }
--- src/timer/Main.java
+++ src/timer/Main.java
@@ -1,10 +1,7 @@
 package timer;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Duration duration = new Duration();
 	}
-
 }
